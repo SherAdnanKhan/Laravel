972cc09f6a81f38e3f9c6cf39c61a3bbc83ec501
Abdul@12345 <abdul.beyond80@gmail.com>
2022-03-05 23:06:11 +0500

    redirect to companies

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = 'log/audit';
+    public const HOME = 'companies';
 
     /**
      * The controller namespace for the application.
